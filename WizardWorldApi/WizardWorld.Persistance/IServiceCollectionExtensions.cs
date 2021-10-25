3adf4faebfb778151b7c16ac33cb5500b6b5aa70
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-10-25 12:53:02 +0200

    Add database config

---
+++ WizardWorldApi/WizardWorld.Persistance/IServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WizardWorld.Persistance
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void AddApplicationDbContext(this IServiceCollection serviceExtensions, string connectionString) {
+            serviceExtensions.AddDbContext<ApplicationDbContext>( options => options.UseNpgsql(connectionString));
+        }
+    }
+}
--- WizardWorldApi/WizardWorldApi/Startup.cs
+++ WizardWorldApi/WizardWorldApi/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WizardWorld.Persistance;
 
 namespace WizardWorldApi
 {
@@ -26,7 +27,7 @@ namespace WizardWorldApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddApplicationDbContext(Configuration.GetConnectionString(Configuration["DefaultConnection"]));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
