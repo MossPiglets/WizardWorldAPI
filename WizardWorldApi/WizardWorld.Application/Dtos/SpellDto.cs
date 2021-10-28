c673fde3ef9df37b51d0cefff22aed5be6f7e3b9
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-10-28 10:32:26 +0200

    SpellDto

---
+++ WizardWorldApi/WizardWorld.Application/Dtos/SpellDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WizardWorld.Persistance.Models.Spells;
+
+namespace WizardWorld.Application.Dtos {
+    public class SpellDto {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Effect { get; set; }
+        public bool CanBeVerbal { get; set; }
+        public SpellType Type { get; set; }
+        public SpellLight Light { get; set; }
+        public string Creator { get; set; }
+    }
+}
