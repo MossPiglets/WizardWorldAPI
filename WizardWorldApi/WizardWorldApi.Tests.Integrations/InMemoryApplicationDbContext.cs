38e681a9d60f71a98903cfe77515159b73fe6517
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-11-09 17:02:48 +0100

    no message

---
+++ WizardWorldApi/WizardWorldApi.Tests.Integrations/InMemoryApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WizardWorld.Persistance.Models.Spells;
+
+namespace WizardWorldApi.Tests.Integrations {
+	public class InMemoryApplicationDbContext : DbContext {
+		public DbSet<Spell> Spells { get; set; }
+		public InMemoryApplicationDbContext(DbContextOptions<InMemoryApplicationDbContext> options) : base(options) { }
+	}
+}
---
+++ WizardWorldApi/WizardWorldApi.Tests.Integrations/WizardWorldWebApplicationFactory.cs
@@ -0,0 +1,7 @@
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace WizardWorldApi.Tests.Integrations
+{
+    public class WizardWorldWebApplicationFactory : WebApplicationFactory<Startup> { }
+}
