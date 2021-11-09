7ebe822f27a007d4ae1f36e115c7d1deeaa7b682
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-11-09 16:18:56 +0100

    GetSpells

---
+++ WizardWorldApi/WizardWorld.Application/Requests/Spells/Queries/GetSpells/GetSpellsQuery.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+using MediatR.AspNet;
+
+namespace WizardWorld.Application.Requests.Spells.GetSpells {
+	public class GetSpellsQuery : IQuery<List<SpellDto>> { }
+}
---
+++ WizardWorldApi/WizardWorldApi/Controllers/SpellController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WizardWorld.Application.Requests.Spells;
+using WizardWorld.Application.Requests.Spells.GetSpells;
+
+namespace WizardWorldApi.Controllers {
+	[ApiController]
+	[Route("[controller]")]
+	public class SpellController : ControllerBase {
+		private readonly IMediator _mediator;
+
+		public SpellController(IMediator mediator) {
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		public async Task<List<SpellDto>> Get() {
+			var query = new GetSpellsQuery();
+			return await _mediator.Send(query);
+		}
+	}
+}
