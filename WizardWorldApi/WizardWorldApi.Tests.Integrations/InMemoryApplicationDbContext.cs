c80bb680587a565426c7900ea12a8b759701f1d5
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-11-15 11:11:35 +0100

    Add Seeder

--- WizardWorldApi/WizardWorldApi.Tests.Integrations/InMemoryApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using WizardWorld.Persistance.Models.Spells;
-
-namespace WizardWorldApi.Tests.Integrations {
-	public class InMemoryApplicationDbContext : DbContext {
-		public DbSet<Spell> Spells { get; set; }
-		public InMemoryApplicationDbContext(DbContextOptions<InMemoryApplicationDbContext> options) : base(options) { }
-	}
-}
