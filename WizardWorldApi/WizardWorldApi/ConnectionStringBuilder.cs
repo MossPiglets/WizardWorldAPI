59361a62346370f641253f6e3a85f303d33b50a5
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-11-15 18:29:06 +0100

    no message

---
+++ WizardWorldApi/WizardWorldApi/ConnectionStringBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Npgsql;
+
+namespace WizardWorldApi {
+    public static class ConnectionStringBuilder {
+        public static string Build() {
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            var builder = new NpgsqlConnectionStringBuilder {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/')
+            };
+
+            return builder.ToString();
+        }
+    }
+}
--- WizardWorldApi/WizardWorldApi/Startup.cs
+++ WizardWorldApi/WizardWorldApi/Startup.cs
@@ -1,32 +1,31 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using WizardWorld.Application;
 using WizardWorld.Persistance;
 
 
 namespace WizardWorldApi {
     public class Startup {
-        public Startup(IConfiguration configuration) {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env) {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
+        private IWebHostEnvironment _env { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddApplicationDbContext(Configuration.GetConnectionString("DefaultConnection"));
+            if (_env.IsProduction()) {
+                services.AddApplicationDbContext(ConnectionStringBuilder.Build());
+            }
+            else {
+                services.AddApplicationDbContext(Configuration.GetConnectionString("DefaultConnection"));
+            }
             services.AddControllers();
             services.AddAppAutoMapper();
             services.AddSwaggerGen(c => {
@@ -36,6 +35,7 @@ namespace WizardWorldApi {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WizardWorldApi v1"));
