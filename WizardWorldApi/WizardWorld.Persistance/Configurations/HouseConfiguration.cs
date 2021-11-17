8d79152d5ee1c540223bb8ef19c08f2bfb8b2c65
Zofia Szybalska <zochula111@gmail.com>
2021-11-17 11:39:53 +0100

    HuseConfiguration

---
+++ WizardWorldApi/WizardWorld.Persistance/Configurations/HouseConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WizardWorld.Persistance.Models.Houses;
+
+namespace WizardWorld.Persistance.Configurations {
+    class HouseConfiguration : IEntityTypeConfiguration<House> {
+        public void Configure(EntityTypeBuilder<House> builder) {
+            builder
+                .HasKey(a => a.Id);
+        }
+    }
+}
