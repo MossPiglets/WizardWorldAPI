b73726eea056d1b19a20932288452742e800085b
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-11-23 11:48:00 +0100

    heroku connection tring fixe

--- WizardWorldApi/WizardWorldApi/HerokuConnectingString.cs
+++ WizardWorldApi/WizardWorldApi/HerokuConnectingString.cs
@@ -17,7 +17,9 @@ namespace WizardWorldApi {
                 Port = databaseUri.Port,
                 Username = userInfo[0],
                 Password = userInfo[1],
-                Database = databaseUri.LocalPath.TrimStart('/')
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
             };
 
             return builder.ToString();
