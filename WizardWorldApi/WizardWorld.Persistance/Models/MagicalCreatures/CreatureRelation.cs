6c942390d9349a12406e49e78e3f3d636e78ec7e
Hubert Morawski <morasiu2@gmail.com>
2021-11-23 19:37:21 +0100

    Update CreatureRelation.cs

--- WizardWorldApi/WizardWorld.Persistance/Models/MagicalCreatures/CreatureRelation.cs
+++ WizardWorldApi/WizardWorld.Persistance/Models/MagicalCreatures/CreatureRelation.cs
@@ -1,4 +1,6 @@
-﻿namespace WizardWorld.Persistance.Models.MagicalCreatures {
+using System;
+
+namespace WizardWorld.Persistance.Models.MagicalCreatures {
     public class CreatureRelation {
         public Guid CreatureId { get; set; }
         public Guid RelatedCreatureId { get; set; }
