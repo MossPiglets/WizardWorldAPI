82a50b8514c821cdd262b6c3fc76325cd55161a8
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-11-24 12:35:48 +0100

    enums

--- WizardWorldApi/WizardWorld.Persistance/Models/Spells/SpellLight.cs
+++ WizardWorldApi/WizardWorld.Persistance/Models/Spells/SpellLight.cs
@@ -4,8 +4,17 @@ namespace WizardWorld.Persistance.Models.Spells {
     public enum SpellLight {
         None = 0,
         Blue = 1,
-        [Description("Icy blue")]
         IcyBlue = 2,
         Red = 3,
+        Gold = 4,
+        Purple = 5,
+        Transparent = 6,
+        White = 7,
+        Green = 8,
+        Orange = 9,
+        Yellow = 10,
+        BrightBlue = 11,
+        Pink = 12,
+        Violet = 13,
     }
 }
--- WizardWorldApi/WizardWorld.Persistance/Models/Spells/SpellType.cs
+++ WizardWorldApi/WizardWorld.Persistance/Models/Spells/SpellType.cs
@@ -2,6 +2,14 @@
     public enum SpellType {
         None = 0,
         Charm = 1,
-        Conjuration = 2
+        Conjuration = 2,
+        Spell = 3,
+        Transfiguration = 4,
+        HealingSpell = 5,
+        DarkCharm = 6,
+        Jinx = 7,
+        Curse = 8,
+        MagicalTransportation = 9,
+        Hex = 10
     }
 }
