8f8a0c5def0ceefbb0d8f3e9600c9a266af015c0
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-11-26 10:08:05 +0100

    enums

--- WizardWorldApi/WizardWorld.Persistance/Models/Spells/SpellLight.cs
+++ WizardWorldApi/WizardWorld.Persistance/Models/Spells/SpellLight.cs
@@ -1,20 +1,28 @@
-﻿using System.ComponentModel;
-
-namespace WizardWorld.Persistance.Models.Spells {
-    public enum SpellLight {
-        None = 0,
-        Blue = 1,
-        IcyBlue = 2,
-        Red = 3,
-        Gold = 4,
-        Purple = 5,
-        Transparent = 6,
-        White = 7,
-        Green = 8,
-        Orange = 9,
-        Yellow = 10,
-        BrightBlue = 11,
-        Pink = 12,
-        Violet = 13,
-    }
+﻿namespace WizardWorld.Persistance.Models.Spells {
+	public enum SpellLight {
+		None = 0,
+		Blue = 1,
+		IcyBlue = 2,
+		Red = 3,
+		Gold = 4,
+		Purple = 5,
+		Transparent = 6,
+		White = 7,
+		Green = 8,
+		Orange = 9,
+		Yellow = 10,
+		BrightBlue = 11,
+		Pink = 12,
+		Violet = 13,
+		BlueishWhite = 14,
+		Silver = 15,
+		Scarlet = 16,
+		Fire = 17,
+		FieryScarlet = 18,
+		Grey = 19,
+		DarkRed = 20,
+		Turquoise = 21,
+		PsychedelicTransparentWave = 22,
+		BrightYellow = 23
+	}
 }
--- WizardWorldApi/WizardWorld.Persistance/Models/Spells/SpellType.cs
+++ WizardWorldApi/WizardWorld.Persistance/Models/Spells/SpellType.cs
@@ -10,6 +10,11 @@
         Jinx = 7,
         Curse = 8,
         MagicalTransportation = 9,
-        Hex = 10
+        Hex = 10,
+        CounterSpell = 11,
+        DarkArts = 12,
+        CounterJinx = 13,
+        CounterCharm = 14,
+        Untransfiguration = 15,
     }
 }
