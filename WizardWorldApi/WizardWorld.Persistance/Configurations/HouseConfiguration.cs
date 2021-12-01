9cbb5734eff65de6c1e14c799e86aa39eebb7050
Zofia Szybalska <zochula111@gmail.com>
2021-12-01 12:03:55 +0100

    Added HasForeignKey

--- WizardWorldApi/WizardWorld.Persistance/Configurations/HouseConfiguration.cs
+++ WizardWorldApi/WizardWorld.Persistance/Configurations/HouseConfiguration.cs
@@ -5,8 +5,8 @@ using WizardWorld.Persistance.Models.Houses;
 namespace WizardWorld.Persistance.Configurations {
     public class HouseConfiguration : IEntityTypeConfiguration<House> {
         public void Configure(EntityTypeBuilder<House> builder) {
-            builder.HasMany(h => h.Traits);
-            builder.HasMany(h => h.Heads);
+            builder.HasMany(h => h.Traits).WithOne().HasForeignKey(t => t.HouseId); ;
+            builder.HasMany(h => h.Heads).WithOne().HasForeignKey(h => h.HouseId);
             builder.HasKey(a => a.Id);
         }
     }
