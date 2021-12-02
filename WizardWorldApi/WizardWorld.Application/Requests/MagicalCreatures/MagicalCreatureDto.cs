8529512551860bc7a897189ced87cdbe3d33fd72
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-12-02 12:38:45 +0100

    Added MagicalCreatureDto

---
+++ WizardWorldApi/WizardWorld.Application/Requests/MagicalCreatures/MagicalCreatureDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using WizardWorld.Persistance.Models.MagicalCreatures;
+
+namespace WizardWorld.Application.Requests.MagicalCreatures {
+    class MagicalCreatureDto {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public CreatureClassificationByMinistryOfMagic Classification { get; set; }
+        public CreatureStatus Status { get; set; }
+        public CreatureDangerLevel DangerLevel { get; set; }
+        public ICollection<CreatureRelation> CreatureRelations { get; set; }
+        public string NativeTo { get; set; }
+    }
+}
