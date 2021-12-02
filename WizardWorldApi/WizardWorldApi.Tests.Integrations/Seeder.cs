e0774e328a19da88ad94c88b99cfa0a2c5d495f1
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-12-02 17:08:19 +0100

    Finished integration tests

--- WizardWorldApi/WizardWorldApi.Tests.Integrations/Seeder.cs
+++ WizardWorldApi/WizardWorldApi.Tests.Integrations/Seeder.cs
@@ -5,6 +5,7 @@ namespace WizardWorldApi.Tests.Integrations {
     public static class Seeder {
         public static void Seed(ApplicationDbContext context) {
             context.AddRange(SpellsGenerator.Spells);
+            context.AddRange(MagicalCreaturesGenerator.MagicalCreatures);
             context.SaveChanges();
         }
     }
--- WizardWorldApi/WizardWorldApi.Tests.Integrations/Tests/MagicalCreatureControllerTests.cs
+++ WizardWorldApi/WizardWorldApi.Tests.Integrations/Tests/MagicalCreatureControllerTests.cs
@@ -1,4 +1,63 @@
-﻿namespace WizardWorldApi.Tests.Integrations.Tests {
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WizardWorld.Application.Requests.MagicalCreatures;
+using WizardWorldApi.Tests.Integrations.Extensions;
+using WizardWorldApi.Tests.Integrations.Generators;
+
+namespace WizardWorldApi.Tests.Integrations.Tests {
     class MagicalCreatureControllerTests {
+        private HttpClient _client;
+
+        [OneTimeSetUp]
+        public void Setup() {
+            var factory = new WizardWorldWebApplicationFactory();
+            _client = factory.CreateClient();
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp() {
+            _client.Dispose();
+        }
+
+        [Test]
+        public async Task Get_ShouldReturnMagicalCreaturesList() {
+            // Arrange
+            var expectedCreatures = MagicalCreaturesGenerator.MagicalCreatures;
+            // Act 
+            var response = await _client.GetAsync("MagicalCreature");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var magicalCreatures = await response.Content.DeserializeAsync<List<MagicalCreatureDto>>();
+            // Assert
+            magicalCreatures.Should().NotBeEmpty();
+            magicalCreatures.Should().BeEquivalentTo(expectedCreatures);
+        }
+
+        [Test]
+        public async Task GetById_ShouldReturnMagicalCreaturesList() {
+            // Arrange
+            var expectedCreature = MagicalCreaturesGenerator.MagicalCreatures.First();
+            // Act 
+            var response = await _client.GetAsync($"MagicalCreature/{expectedCreature.Id}");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var spell = await response.Content.DeserializeAsync<MagicalCreatureDto>();
+            // Assert
+            spell.Should().BeEquivalentTo(expectedCreature);
+        }
+
+        [Test]
+        public async Task GetById_NotExistingId_ShouldReturnNotFound() {
+            // Arrange
+            var notExistingId = Guid.NewGuid();
+            // Act 
+            var response = await _client.GetAsync($"MagicalCreature/{notExistingId}");
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
     }
 }
