a9ee6af8ff31462333b94124be2d80d214e27101
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-12-02 12:43:03 +0100

    Created Map

---
+++ WizardWorldApi/WizardWorld.Application/Requests/MagicalCreatures/MagicalCreatureMappingProfile.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+using WizardWorld.Persistance.Models.MagicalCreatures;
+
+namespace WizardWorld.Application.Requests.MagicalCreatures {
+    class MagicalCreatureMappingProfile : Profile {
+        public MagicalCreatureMappingProfile() {
+            CreateMap<MagicalCreature, MagicalCreatureDto>();
+        }
+    }
+}
