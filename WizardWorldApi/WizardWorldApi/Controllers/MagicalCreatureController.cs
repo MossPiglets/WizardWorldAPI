53ce5003b52c929184ea3505912716c977384454
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-12-02 14:25:49 +0100

    Created controller

---
+++ WizardWorldApi/WizardWorldApi/Controllers/MagicalCreatureController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WizardWorld.Application.Requests.MagicalCreatures;
+using WizardWorld.Application.Requests.MagicalCreatures.Queries.GetMagicalCreatureById;
+using WizardWorld.Application.Requests.MagicalCreatures.Queries.GetMagicalCreatures;
+
+namespace WizardWorldApi.Controllers {
+        [ApiController]
+        [Route("[controller]")]
+        public class MagicalCreatureController : ControllerBase {
+            private readonly IMediator _mediator;
+
+            public MagicalCreatureController(IMediator mediator) {
+                _mediator = mediator;
+            }
+
+            [HttpGet]
+            public async Task<List<MagicalCreatureDto>> Get([FromQuery] GetMagicalCreaturesQuery query) {
+                return await _mediator.Send(query);
+            }
+
+            [HttpGet("{id}")]
+            public async Task<MagicalCreatureDto> GetById([FromRoute] Guid id) {
+                var query = new GetMagicalCreatureByIdQuery {
+                    Id = id
+                };
+                return await _mediator.Send(query);
+            }
+        }
+    }
+}
