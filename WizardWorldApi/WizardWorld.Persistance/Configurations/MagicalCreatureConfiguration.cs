435612071bb87d03220137770519ca1b743983fa
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-12-02 12:17:23 +0100

    Configuration

---
+++ WizardWorldApi/WizardWorld.Persistance/Configurations/MagicalCreatureConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WizardWorld.Persistance.Models.MagicalCreatures;
+
+namespace WizardWorld.Persistance.Configurations {
+    public class MagicalCreatureConfiguration : IEntityTypeConfiguration<MagicalCreature> {
+        public void Configure(EntityTypeBuilder<MagicalCreature> builder) {
+            builder.HasMany(h => h.CreatureRelations).WithOne().HasForeignKey(t => t.CreatureId); ;
+            builder.HasKey(a => a.Id);
+        }
+    }
+}
