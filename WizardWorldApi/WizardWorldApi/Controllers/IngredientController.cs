5b8ebd480c28fcae4af75fd9ac792587ac730f6c
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-12-04 14:23:18 +0100

    Controllers

---
+++ WizardWorldApi/WizardWorldApi/Controllers/IngredientController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WizardWorld.Application.Requests.Ingredients;
+using WizardWorld.Application.Requests.Ingredients.Queries.GetIngredientById;
+using WizardWorld.Application.Requests.Ingredients.Queries.GetIngredients;
+
+namespace WizardWorldApi.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class IngredientController : ControllerBase {
+        private readonly IMediator _mediator;
+
+        public IngredientController(IMediator mediator) {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<List<IngredientDto>> Get([FromQuery] GetIngredientsQuery query) {
+            return await _mediator.Send(query);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IngredientDto> GetById([FromRoute] Guid id) {
+            var query = new GetIngredientByIdQuery() {
+                Id = id
+            };
+            return await _mediator.Send(query);
+        }
+        
+    }
+}
---
+++ WizardWorldApi/WizardWorldApi/Controllers/WizardController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WizardWorld.Application.Requests.Wizards;
+using WizardWorld.Application.Requests.Wizards.Queries.GetWizardById;
+using WizardWorld.Application.Requests.Wizards.Queries.GetWizards;
+
+namespace WizardWorldApi.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class WizardController : ControllerBase {
+        private readonly IMediator _mediator;
+        public WizardController(IMediator mediator) {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<List<WizardDto>> Get([FromQuery] GetWizardsQuery query) {
+            return await _mediator.Send(query);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<WizardDto> GetById([FromRoute] Guid id) {
+            var query = new GetWizardByIdQuery() {
+                Id = id
+            };
+            return await _mediator.Send(query);
+        }
+    }
+}
