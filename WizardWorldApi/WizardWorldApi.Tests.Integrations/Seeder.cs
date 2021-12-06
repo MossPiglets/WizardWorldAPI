f9b402abd213750eb282550bb78ff1d687c33cf7
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-12-06 11:40:05 +0100

    add to seeder

--- WizardWorldApi/WizardWorldApi.Tests.Integrations/Seeder.cs
+++ WizardWorldApi/WizardWorldApi.Tests.Integrations/Seeder.cs
@@ -5,6 +5,9 @@ namespace WizardWorldApi.Tests.Integrations {
     public static class Seeder {
         public static void Seed(ApplicationDbContext context) {
             context.AddRange(SpellsGenerator.Spells);
+            context.AddRange(IngredientsGenerator.Ingredients);
+            context.AddRange(ElixirsGenerator.Elixirs);
+            context.AddRange(WizardsGenerator.Wizards);
             context.SaveChanges();
         }
     }
---
+++ WizardWorldApi/WizardWorldApi.Tests.Integrations/Tests/IngredientControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using WizardWorld.Application.Requests.Ingredients;
+using WizardWorldApi.Tests.Integrations.Extensions;
+using WizardWorldApi.Tests.Integrations.Generators;
+
+namespace WizardWorldApi.Tests.Integrations.Tests {
+    public class IngredientControllerTests {
+        private HttpClient _client;
+
+        [OneTimeSetUp]
+        public void Setup() {
+            var factory = new WizardWorldWebApplicationFactory();
+            _client = factory.CreateClient();
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp() {
+            _client.Dispose();
+        }
+        [Test]
+        public async Task Get_NoQueryParameters_ShouldReturnIngredientsList() {
+            // Arrange
+            var expectedIngredients = IngredientsGenerator.Ingredients;
+            // Act 
+            var response = await _client.GetAsync("Ingredient");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var ingredients = await response.Content.DeserializeAsync<List<IngredientDto>>();
+            // Assert
+            ingredients.Should().NotBeEmpty();
+            ingredients.Should().BeEquivalentTo(expectedIngredients);
+        }
+    }
+}
