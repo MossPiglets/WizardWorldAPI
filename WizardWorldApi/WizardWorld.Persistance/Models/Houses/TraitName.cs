cdb06fb32d9d415c077824c3c4ba6d0f4afb3812
Zofia Szybalska <zochula111@gmail.com>
2022-01-11 18:45:43 +0100

    Added trait enum

--- WizardWorldApi/WizardWorld.Persistance/Models/Houses/TraitName.cs
+++ WizardWorldApi/WizardWorld.Persistance/Models/Houses/TraitName.cs
@@ -1,6 +1,29 @@
 ﻿namespace WizardWorld.Persistance.Models.Houses {
     public enum TraitName {
-        None = 0
+        None = 0,
+        Courage = 1,
+        Bravery = 2,
+        Determination = 3,
+        Daring = 4,
+        Nerve = 5,
+        Chivalary = 6,
+        Hardworking = 7,
+        Patience = 8,
+        Fairness = 9,
+        Just = 10,
+        Loyalty = 11,
+        Modesty = 12,
+        Wit = 13,
+        Learning = 14,
+        Wisdom = 15,
+        Acceptance = 16,
+        Inteligence = 17,
+        Creativity = 18,
+        Resourcefulness = 19,
+        Pride = 20,
+        Cunning = 21,
+        Ambition = 22,
+        Selfpreservation = 23,
     }
 
 }
